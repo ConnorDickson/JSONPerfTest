43355ac99bcba8cc0d7844d210c2fa1b3e79d12c
Connor Dickson <cdickson14@qub.ac.uk>
2020-08-12 06:48:25 +0100

    Init commit

---
+++ JSONProcessing/JSONProcessing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace JSONProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            var jsonPath = "json.json";
+            var loopsToAverage = 10;
+            var jsonFileText = File.ReadAllText(jsonPath);
+
+            Console.WriteLine("Executing newtonsoft loop");
+            stopwatch.Start();
+
+            for(int i = 0; i < loopsToAverage; i++)
+            {
+                var newtonsoftLoadedJson = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(jsonFileText);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Newtonsoft elapsed seconds = {stopwatch.Elapsed.TotalSeconds}");
+            stopwatch.Reset();
+
+            Console.WriteLine("Executing System.text.json loop");
+            stopwatch.Start();
+
+            for (int i = 0; i < loopsToAverage; i++)
+            {
+                var systemLoadedJson = System.Text.Json.JsonSerializer.Deserialize<object>(jsonFileText);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"System elapsed seconds = {stopwatch.Elapsed.TotalSeconds}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# JSONPerfTest
-Testing the difference between Newtonsoft and System.Text.Json
+# Newtonsoft vs System.Text.Json
+
+This is a quick test to check the performance impact of Newtonsoft vs System.Text.Json.
+
+Please find the bl blog here: 
